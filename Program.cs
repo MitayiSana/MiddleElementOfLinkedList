5033b1618dba6a0abaa7f994912c99cdf70e455a
MitayiSana <mitayisana@gmail.com>
2024-10-17 13:36:11 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace MiddleElementOfLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> list = new LinkedList<int>();
+            list.AddLast(10);
+            list.AddLast(20);
+            list.AddLast(30);
+            list.AddLast(40);
+            LinkedList<int> result = new LinkedList<int>();
+            int res=0;
+         
+                res = list.Count/2;
+            Console.WriteLine(list.ElementAt<int>(res));
+
+           
+
+
+
+        }
+    }
+}
